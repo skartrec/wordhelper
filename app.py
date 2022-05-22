4be6447d7712ec1a2bc9f5f5248df38a08405cbb
skartrec <mattias.skarec@itfokus.se>
2022-05-22 21:07:12 +0200

    testing excludefunc

--- app.py
+++ app.py
@@ -3,27 +3,26 @@ from flask import request
 from flask import redirect
 from flask import jsonify
 from flask import Flask, render_template
-from functions import new_words
+from functions import validate, wordsearch, truncatelist, exclude
 
 app = Flask(__name__,template_folder='templates')
 
+complete = []
+
 @app.route('/', methods=['GET','POST'])
 def index():
-    from functions import validate, wordsearch, truncatelist
+    # from functions import validate, wordsearch, truncatelist, exclude, new_words
     if request.method == 'POST':
-        form = request.form
-        default_value = 'man'
-        exclude = request.form.get('exclude', default_value)
         validate()
         wordsearch()
         truncatelist()
+        exclude(complete)
         return redirect('result.html')
     return(render_template('index.html'))
 
 @app.route('/result.html', methods=['GET','POST'])
 def result():
-    return(render_template('result.html', new_words = new_words))
-    
+    return(render_template('result.html', complete = complete))
 
 if __name__ == "__main__":
     app.run(port='8088',threaded=True)
--- functions.py
+++ functions.py
@@ -1,33 +1,41 @@
 from flask import Flask
 import re
 
-# complete_words = [x for x in words if len(x) <= 6]
 words = []
 new_words = []
 
 def validate():
     words.clear()
     from app import request
-    form = request.form
-    for v in form.values(): 
-        print(v)
+    form = request.form.getlist('text')
+    for v in form: 
         if v == '':
             words.append(".")
         else:
             words.append(v.lower())
-    print(words) 
 
 def wordsearch():
+    from app import request
     new_words.clear()
-    print(new_words)
     file = open('svenska-ord.txt', encoding='utf-8')
     pattern = f'^{words[0]}{words[1]}{words[2]}{words[3]}{words[4]}{words[5]}$'
     for line in file.readlines():
         if re.findall(pattern, line.rstrip('\n')):
             line = new_words.append(line.rstrip('\n'))
-            print(new_words)
+
+def exclude(list):
+    from app import request
+    exclude = request.form['exclude']
+    excluded_words =  [ele for ele in new_words if all(ch not in ele for ch in exclude)]
+    print(excluded_words)
+
 
 def truncatelist():
     if len(new_words) >= 25:
         del new_words[25:]
+
+# def exclude_letters():
+#     from app import request
+#     exclude = request.form['exclude']
+#     excluded_words =  [ele for ele in new_words if all(ch not in ele for ch in exclude)]
 
